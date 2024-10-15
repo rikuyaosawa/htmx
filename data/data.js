01aae1dc92b0af06cb4f3c2319ae9984117a0bd5
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-10-15 15:02:05 +0900

    add book data

---
+++ data/data.js
@@ -0,0 +1,26 @@
+const BOOK_DATA = [
+  {
+    id: 1,
+    title: "Moral Letters to Lucilius",
+    author: "Seneca",
+    content: [
+      {
+        subheading: "Letter 1",
+        pageNo: 1,
+        text: "Continue to act thus, my dear Lucilius – set yourself free for your own sake; gather and save your time, which till lately has been forced from you, filched away, or has merely slipped from your hands. Make yourself believe the truth of my words: certain moments are torn from us, some are gently removed, and others glide beyond our reach. The most disgraceful kind of loss, however, is due to carelessness. Furthermore, if you pay close heed to the problem, you will find that a large portion of our life passes while we are doing ill, a goodly share while we are doing nothing, and the whole while we are engaged in that which is not purposeful. What man can you show me who places any value on his time, who reckons the worth of each day, who understands that he is dying daily? For we are mistaken when we look forward to death; the major portion of death has already passed. Whatever years lie behind us are in death's hands.",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Hello World",
+    author: "Rikuya Osawa",
+    content: {
+      subheading: "",
+      pageNo: 1,
+      text: "Hello, world! This is Rikuya Osawa.",
+    },
+  },
+];
+
+export default BOOK_DATA;
