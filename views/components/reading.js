c80fb103303b1c639bcc609472bde8f08df1f3b7
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-10-16 16:30:24 +0900

    refactor: return different template based on the condition

--- views/components/reading.js
+++ views/components/reading.js
@@ -1,37 +1,41 @@
 function createReadingTemplate(book, pageNo) {
-    let backButton = "",
-        nextButton = "";
     const index = pageNo - 1;
-
-    if (pageNo != 1 && pageNo != book.content.length) {
-        nextButton = /*html*/ `<button
-			hx-get="/book/${book.id}/${pageNo}"
-			hx-vals='{"buttonType":"next"}'
-			hx-target="main">Next</button>`;
-        backButton = /*html*/ `<button
-			hx-get="/book/${book.id}/${pageNo}"
-			hx-vals='{"buttonType":"back"}'
-			hx-target="main">Back</button>`;
-    } else if (pageNo !== book.content.length) {
-        nextButton = /*html*/ `<button
-			hx-get="/book/${book.id}/${pageNo}"
-			hx-vals='{"buttonType":"next"}'
-			hx-target="main">Next</button>`;
-    } else if (pageNo !== 1 && pageNo === book.content.length) {
-        backButton = /*html*/ `<button
-			hx-get="/book/${book.id}/${pageNo}"
-			hx-vals='{"buttonType":"back"}'
-			hx-target="main">Back</button>`;
-    }
-
-    return /*html*/ `
+    const bookTemplate = /*html*/ `
         <h1>${book.title}</h1>
         <p>by ${book.author}</p>
         <h3>${book.content[index].subheading}</h3>
         <p>${book.content[index].text}</p>
-        <p>Page ${book.content[index].pageNo}/${book.content.length}</p>
-		${backButton}
-		${nextButton}`;
+        <p>Page ${book.content[index].pageNo}/${book.content.length}</p>`;
+
+    const nextButton = /*html*/ `<button
+			hx-get="/book/${book.id}/${pageNo}"
+			hx-vals='{"buttonType":"next"}'
+			hx-target="main">Next</button>`;
+    const backButton = /*html*/ `<button
+			hx-get="/book/${book.id}/${pageNo}"
+			hx-vals='{"buttonType":"back"}'
+			hx-target="main">Back</button>`;
+
+    const isNotFirstOrLastPage = pageNo != 1 && pageNo != book.content.length;
+    const isNotLastPage = pageNo !== book.content.length;
+    const isLastPageAndNotFirstPage =
+        pageNo !== 1 && pageNo === book.content.length;
+    if (isNotFirstOrLastPage) {
+        return /*html*/ `
+            ${bookTemplate}
+            ${backButton}
+            ${nextButton}`;
+    } else if (isNotLastPage) {
+        return /*html*/ `
+            ${bookTemplate}
+            ${nextButton}`;
+    } else if (isLastPageAndNotFirstPage) {
+        return /*html*/ `
+            ${bookTemplate}
+            ${backButton}`;
+    } else {
+        return /*html*/ `${bookTemplate}`;
+    }
 }
 
 export default createReadingTemplate;
