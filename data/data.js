c4fee16a63f84b9cbf96944e0b896932cc6bf884
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-10-16 10:12:33 +0900

    add page 2 for test

--- data/data.js
+++ data/data.js
@@ -9,6 +9,11 @@ const BOOK_DATA = [
         pageNo: 1,
         text: "Continue to act thus, my dear Lucilius – set yourself free for your own sake; gather and save your time, which till lately has been forced from you, filched away, or has merely slipped from your hands. Make yourself believe the truth of my words: certain moments are torn from us, some are gently removed, and others glide beyond our reach. The most disgraceful kind of loss, however, is due to carelessness. Furthermore, if you pay close heed to the problem, you will find that a large portion of our life passes while we are doing ill, a goodly share while we are doing nothing, and the whole while we are engaged in that which is not purposeful. What man can you show me who places any value on his time, who reckons the worth of each day, who understands that he is dying daily? For we are mistaken when we look forward to death; the major portion of death has already passed. Whatever years lie behind us are in death's hands.",
       },
+      {
+        subheading: "Letter 2",
+        pageNo: 2,
+        text: "blah blah blah",
+      },
     ],
   },
   {
