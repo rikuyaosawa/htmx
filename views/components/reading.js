44c8933c52b9b5f8d0c3d60458f9c419eb57cbb7
Osawa Rikuya <rikuyaosawa@gmail.com>
2024-10-16 10:13:20 +0900

    feat: add next page button

--- views/components/reading.js
+++ views/components/reading.js
@@ -1,9 +1,14 @@
 const createReadingTemplate = (book, index) => /*html*/ `
     <h1>${book.title}</h1>
-    <p>${book.author}</p>
+    <p>by ${book.author}</p>
     <h3>${book.content[index].subheading}</h3>
     <p>${book.content[index].text}</p>
-    <p>${book.content[index].pageNo}</p>
+    <p>Page ${book.content[index].pageNo}</p>
+    <button
+        hx-get="/book/${book.id}/${index}"
+        hx-target="main">Next Page</button>
+    <div class="page-no-list">
+    </div>
 `;
 
 export default createReadingTemplate;
