d7dd40d4e600a2408fb0d4b6d7a1cb817f9f9c5a
rikuyaosawa <rikuyaosawa@gmail.com>
2024-10-16 21:15:46 +0900

    add quote data

--- data/quoteData.js
+++ data/quoteData.js
@@ -1,3 +1,11 @@
-const QUOTE_DATA = [{}];
+const QUOTE_DATA = [
+    {
+        id: 1,
+        person: "Seneca",
+        source: "Moral Letters to Lucilius",
+        content:
+            "We are mistaken when we look forward to death; the major portion of death has already passed. Whatever years lie behind us are in death's hands.",
+    },
+];
 
 export default QUOTE_DATA;
